65d02b391ca206cee09398793fe82da438cc856d
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-01-22 18:39:20 +0530

    ADDED: Performing Third Arithmetic Operation

--- Sorting_Arithmetic_Computation.sh
+++ Sorting_Arithmetic_Computation.sh
@@ -10,3 +10,6 @@ echo "The output of First Operation is: $First_Operation"
 
 Second_Operation=$(( $a * $b + $c ))
 echo "The output of Second Operation is: $Second_Operation"
+
+Third_Operation=$(( $c + $( echo "scale=2; $(( $a/$b ))" | bc ) ))
+echo "The output of Third Operation is: $Third_Operation"
