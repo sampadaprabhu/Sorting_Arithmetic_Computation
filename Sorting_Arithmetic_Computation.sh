e96779aca9f2fd1acab2fd935fff716df04123c7
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-01-22 18:24:40 +0530

    ADDED: Performing Second Arithmetic Operation

--- Sorting_Arithmetic_Computation.sh
+++ Sorting_Arithmetic_Computation.sh
@@ -7,3 +7,6 @@ read c
 
 First_Operation=$(( $a + $b * $c ))
 echo "The output of First Operation is: $First_Operation"
+
+Second_Operation=$(( $a * $b + $c ))
+echo "The output of Second Operation is: $Second_Operation"
